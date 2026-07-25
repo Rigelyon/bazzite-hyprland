2ce130451c37196cc0e8a93a4dd4fb6ed93b0c3d
Rigelyon <rigelorionis120@gmail.com>
2026-07-26 00:41:30 +0700

    refactor: remove package exclusion logic for lionheartp repositories in build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -24,13 +24,6 @@ for repo in "${COPR_REPOS[@]}"; do
     dnf5 -y copr enable "$repo"
 done
 
-# Exclude broken Hyprland packages from lionheartp so we only get Noctalia from it
-for repo_file in /etc/yum.repos.d/*lionheartp*Hyprland*.repo; do
-    if [ -f "$repo_file" ]; then
-        echo "exclude=hypr* aquamarine* xdg-desktop-portal-hyprland*" >> "$repo_file"
-    fi
-done
-
 cat <<'EOF' > /etc/yum.repos.d/wayscriber.repo
 [wayscriber]
 name=Wayscriber Repo
